38f3d28c21e989bcae22c916210394428ac61083
Jair Fonseca <jairfonseca64@yahoo.es>
2021-11-27 16:01:00 -0500

    Random apple

--- SnakeProjectNicolasPerdomoHectorFonseca/Program.cpp
+++ SnakeProjectNicolasPerdomoHectorFonseca/Program.cpp
@@ -1,7 +1,13 @@
 #include "ConsoleWriter.h"
+#include "Console.h"
+#include "Random.h"
+
+using utility::Random;
 using utility::ConsoleWriter;
+using utility::Console;
 bool gameOver;
 void program();
+int appleX, appleY;
 
 int main()
 {
@@ -11,8 +17,20 @@ int main()
 
 void program()
 {
-	ConsoleWriter ventana = ConsoleWriter();
-	ventana.setWindowSize(200, 50);
-	ventana.drawRectangle(20, 10, 70, 40);
-	
+		ConsoleWriter windowGame = ConsoleWriter();
+		windowGame.setCursorVisible(false);
+		//windowGame.setWindowSize(200, 50);
+		windowGame.drawRectangle(10, 5, 100, 10);
+		windowGame.drawRectangle(10, 10, 100, 35);
+	while (true)
+	{
+		windowGame.setCursorPosition(0, 0);
+		COORD topLeft = { appleX, appleY };
+		COORD bottomRight = { appleX, appleY };
+		windowGame.clearRegion(topLeft, bottomRight);
+		appleX = Random::getRandomInteger(10 + 1, 100 - 1);
+		appleY = Random::getRandomInteger(10 + 1, 35 - 1);
+		windowGame.setCharacterAtPosition(appleX, appleY, /*254*/ 64);
+		Console::pause();
+	}
 }
