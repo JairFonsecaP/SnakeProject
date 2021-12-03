454f4d7b6c1f8df9c8db64d19c37356604861cdf
nicolas perdomo <nicolasperdomol@hotmail.com>
2021-12-03 17:27:30 -0500

    Class Rectangle added

---
+++ SnakeProjectNicolasPerdomoHectorFonseca/Rectangle.h
@@ -0,0 +1,18 @@
+#pragma once
+#include"Coordinate.h"
+class Rectangle
+{
+private:
+	utility::Coordinate topLeft;
+	utility::Coordinate bottomRight;
+
+public:
+	Rectangle(utility::Coordinate topLeft, utility::Coordinate bottomRight);
+
+	utility::Coordinate getTopRigth();
+	utility::Coordinate getBottomLeft();
+	int getSize();
+	int getWidth();
+	int getHeight();
+};
+
